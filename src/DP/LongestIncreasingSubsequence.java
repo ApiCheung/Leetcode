c01ec1435e60d5bbea5443560f25b49300e6f8fc
EsmeeZhang <esmezh42@gmail.com>
2020-07-20 21:59:25 -0400

    PowerofTwo

--- src/DP/LongestIncreasingSubsequence.java
+++ src/DP/LongestIncreasingSubsequence.java
@@ -49,20 +49,22 @@ public class LongestIncreasingSubsequence {
         return res;
     }
 
-    public int lengthOfLIS2(int[] nums){
-        int[] dp = new int[nums.length];
-        int len = 0;
-        for(int num : nums){
-            int i = Arrays.binarySearch(dp, 0, len, num);
-            if(i < 0) i = -(i + 1);
-
-            dp[i] = num;
-            if(i == len){
-                len++;
+    public int lengthOfLIS2(int[] nums) {
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for (int x : nums) {
+            int i = 0, j = size;
+            while (i != j) {
+                int m = (i + j) / 2;
+                if (tails[m] < x)
+                    i = m + 1;
+                else
+                    j = m;
             }
+            tails[i] = x;
+            if (i == size) ++size;
         }
-        return len;
+        return size;
     }
-
 
 }
