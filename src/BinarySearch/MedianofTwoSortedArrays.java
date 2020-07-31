2717567a2cc446ef618e81b606b711e28fbb7a5f
EsmeeZhang <esmezh42@gmail.com>
2020-07-31 17:50:48 -0400

    Create MedianofTwoSortedArrays.java

---
+++ src/BinarySearch/MedianofTwoSortedArrays.java
@@ -0,0 +1,18 @@
+package BinarySearch;
+
+/**
+ * @author Esmee Zhang
+ * @date 7/30/20 9:14 下午
+ * @projectName Leetcode
+ */
+public class MedianofTwoSortedArrays {
+    /**
+     * TIME O(LOG(M+N)
+     *
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 1.0;
+
+    }
+
+}
