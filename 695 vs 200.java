de83fdbd2cde16d0fd32a04f08b309f24ebc8c4e
Esmee Zhang <60492337+Esmee-Zhang@users.noreply.github.com>
2020-10-25 18:05:57 -0400

    Update 695 vs 200.java

--- 695 vs 200.java
+++ 695 vs 200.java
@@ -1,6 +1,6 @@
 
   
-//695 vs 200
+//695 vs 200 vs 463
 //max area of island
 
 class Solution {
@@ -45,6 +45,36 @@ class Solution {
         dfs(grid, x - 1, y);
         dfs(grid, x, y + 1);
         dfs(grid, x, y - 1);
+    }
+}
+
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        if(grid == null) return 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1) return dfs(grid, i, j);
+            }
+        }
+        return 0;
+        
+    }
+    
+    public int dfs(int[][] grid, int i, int j){
+        if(i < 0 || i>= grid.length || j < 0 || j >= grid[0].length) return 1;
+        
+        if(grid[i][j] == 0) return 1;
+        
+        if(grid[i][j] == -1) return 0;
+        
+        int count = 0;
+        grid[i][j] = -1;
+        
+        count += dfs(grid, i+1, j);
+        count += dfs(grid, i-1, j);
+        count += dfs(grid, i, j+1);
+        count += dfs(grid, i, j-1);
+        return count;
     }
 }
 
