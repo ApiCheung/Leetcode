72c7c758fa9efe64554ee2511edd6c807f2abf8a
Esmee Zhang <60492337+Esmee-Zhang@users.noreply.github.com>
2020-10-27 13:50:57 -0400

    Update and rename 347.java to 347 vs 451.java

--- 347 vs 451.java
+++ 347 vs 451.java
@@ -1,4 +1,4 @@
-//347
+//347 vs 451
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
@@ -32,6 +32,35 @@ class Solution {
         return res;
         
         
+        
+    }
+}
+
+
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        List<Character>[] buck = new List[s.length() + 1];
+        
+        for(char key : map.keySet()){
+            int freq = map.get(key);
+            if(buck[freq] == null) buck[freq] = new ArrayList<>();
+            buck[freq].add(key);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int pos = buck.length - 1; pos >=0 ; pos--){
+            if(buck[pos] != null)
+                for (char c : buck[pos])
+                    for(int i=0; i < pos; i++)
+                        sb.append(c);
+        }
+        
+        return sb.toString();
         
     }
 }
