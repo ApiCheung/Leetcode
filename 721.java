6b8ab20f9ba0f160b780ed5e6e3ccea1e6d907e2
Esmee Zhang <60492337+Esmee-Zhang@users.noreply.github.com>
2020-10-28 15:08:57 -0400

    Update 721.java

--- 721.java
+++ 721.java
@@ -7,9 +7,19 @@ class Solution {
         for(List<String> a: accounts){
         	for(int i = 1; i < a.size(); i++){
       			parents.put(a.get(i), a.get(i));//一开始线先设置每一个邮箱的parent都是自己
-      			owner.put(a.get(i), a.get(0));
+      			owner.put(a.get(i), a.get(0));//每个账户和姓名对应
       		}
         }
+        
+        //将parents map中这些邮箱全部用每个账户里面的第一个邮箱作为parents
+        for(List<String> a: accounts){
+            String p = find(a.get(1), parents);
+            for(int i = 2; i < a.size(); i++){
+                parents.put(find(a.get(i), parents), p);
+            }
+        }
+        
+        //建立union set 根据之前parent的来查找
 
         for(List<String> a : accounts){
         	String p = find(a.get(1), parents);
@@ -18,6 +28,7 @@ class Solution {
         		unions.get(p).add(a.get(i));
         }
 
+        //
         List<List<String>> res = new ArrayList<>();
         for(String p : unions.keySet()){
         	List<String> emails = new ArrayList(unions.get(p));
