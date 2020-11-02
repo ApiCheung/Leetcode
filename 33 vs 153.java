721bade6b143583218f36438eb4f14132aad5f60
Esmée Zhang <60492337+Esmee-Zhang@users.noreply.github.com>
2020-11-01 20:37:40 -0500

    Update and rename 33.java to 33 vs 153.java

--- 33 vs 153.java
+++ 33 vs 153.java
@@ -27,3 +27,24 @@ public int search(int[] nums, int target){
 	if(nums[end] == target) return end;
 	return -1;
 }
+
+//153
+
+public int finMin(int[] nums){
+	//same useing the binary search
+	if(nums.length == 0) return 0;
+	int start = 0;
+	int end = nums.length - 1;
+	
+	while( start != end){
+		int mid = (end + start) / 2;
+		if(nums[mid] < nums[end]){
+			end = mid;
+		}else if(nums[mid] > nums[end]){
+			start = mid + 1;
+		}else{
+			end --;
+		}
+	}
+	return nums[start];
+}
